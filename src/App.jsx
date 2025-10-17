10b7282d33dcfc5707f70f478f4c2314a6c0c8f7
JAYATHI <2300030713@kluniversity.in>
2025-10-17 21:19:03 +0530

    git pages

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hi Hello this is frontend to test deployment</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
